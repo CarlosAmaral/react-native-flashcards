8823cd34ad5864c8315d6bc14a16deae13d5d5e7
CarlosAmaral <carloslopesamaral@outlook.com>
2018-10-14 20:51:17 -0400

    added: project views, API methods and antd for react native

---
+++ API.js
@@ -0,0 +1,24 @@
+import {React} from 'react';
+
+/**
+ * GET: fetch all Decks
+ */
+export const getDecks = () => {}
+
+/**
+ * GET: fetch individual Deck
+ */
+export const getDeck = (id) => {}
+
+/**
+ * POST: save individual Deck
+ * @param {*} title 
+ */
+export const saveDeckTitle = (title) => {}
+
+/**
+ * POST: save Card to Deck
+ * @param {*} title 
+ * @param {*} card 
+ */
+export const addCardToDeck = (title, card) => {}
